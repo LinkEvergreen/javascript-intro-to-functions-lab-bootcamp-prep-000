b20f8a817862eac08efb06e92b28dfc1a158a671
David MacPhail <david.macphail@icloud.com>
2020-05-20 19:54:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,18 +6,20 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
+function logShout (string) {
+     console.log(string.toUpperCase())
+}
 
+function logWhisper (string) {
+     console.log(string.toLowerCase())
+}
 
-var uppercase = "HELLO!"
-
-uppercase.toUpperCase() === uppercase // true
-
-var lowercase = 'hello!'
-
-lowercase.toLowerCase() === lowercase // true
-
-var mixedCase = 'Hi there!'
-
-mixedCase.toLowerCase() === mixedCase // false
-
-mixedCase.toUpperCase() === mixedCase // false
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  } else if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+}
